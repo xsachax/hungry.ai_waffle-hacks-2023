15493d87a0f39114542d32406303a57ffe3a5f65
HenrySaber <hsabe026@uottawa.ca>
2023-06-24 16:10:23 -0400

    Added Pictures and footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,17 @@ import Prompt from "../Prompt/Prompt.tsx";
 import "./App.css";
 import Navbar from "../Navbar/Navbar.tsx";
 import Hero from "../Hero/Hero.tsx";
+import Footer from "../Footer/Footer.tsx";
 
 function App() {
   //const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex w-full m-auto">
+      <div className="flex w-full">
         <Prompt />
+        <Footer />
+
       </div>
     </>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="marketing-footer">
+      <div className="footer-content">
+        <h3>Contact Us</h3>
+        <p>123 Main Street, City</p>
+        <p>Email: info@example.com</p>
+        <p>Phone: 123-456-7890</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
