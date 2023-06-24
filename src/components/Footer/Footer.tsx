d55061ba23029a58c376368a105b8bea596c9d53
HenrySaber <hsabe026@uottawa.ca>
2023-06-24 18:16:13 -0400

    Fixed some things

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Footer.css";
 
+
+
 const Footer = () => {
   return (
     <footer className="marketing-footer">
-      <div className="footer-content">
+      <div className="footer-logo">
         <img src="Hungry.Ai.png" alt="Description of the image" />
-        <p>Email: info@example.com</p>
-        <p>Phone: 123-456-7890</p>
+      </div>
+      <div className="footer-content">
+      <p>Serving up culinary creativity with AI precision</p>
       </div>
     </footer>
   );
 };
+
 
 export default Footer;
 
