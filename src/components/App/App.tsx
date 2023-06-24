b1573f3db2f6c4500de110df2f1b05d3a0db7c04
Sacha Arseneault <sacha.arseneault@gmail.com>
2023-06-24 05:00:32 -0400

    chore: remove comments in app.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,17 +11,6 @@ function App() {
     <>
       <div className="flex w-full m-auto">
         <Prompt />
-        {/* 
-      <div className="app">
-        <h1 className="text-3xl font-bold">Hello World</h1>
-        <button
-          className="border border-gray-300 rounded-md px-4 py-2"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-      </div>
-      */}
       </div>
     </>
   );
