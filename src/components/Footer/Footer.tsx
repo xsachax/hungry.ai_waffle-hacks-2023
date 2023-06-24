200f0ef308d15f87fa3a0af1872a724528cbd542
Egzavyer <xavier.lermusieaux@gmail.com>
2023-06-24 18:28:15 -0400

    fixed image not showing up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import "./Footer.css";
 
-
-
 const Footer = () => {
-  return (
-    <footer className="marketing-footer">
-      <div className="footer-logo">
-        <img src="Hungry.Ai.png" alt="Description of the image" />
-      </div>
-      <div className="footer-content">
-      <p>Serving up culinary creativity with AI precision</p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className='marketing-footer'>
+			<div className='footer-content'>
+				<img
+					src='../../../public/Asset/Hungry.Ai.png'
+					alt='Description of the image'
+				/>
+				<p>Email: info@example.com</p>
+				<p>Phone: 123-456-7890</p>
+			</div>
+		</footer>
+	);
 };
 
-
 export default Footer;
-
