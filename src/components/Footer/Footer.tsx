b12c4beb8c10a7895816cfe59c5c83d2c6e939d0
Egzavyer <xavier.lermusieaux@gmail.com>
2023-06-24 18:42:20 -0400

    styled footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,9 +6,13 @@ const Footer = () => {
 		<footer className='marketing-footer'>
 			<div className='footer-content'>
 				<img
+					className='LogoImg'
 					src='../../../public/Asset/Hungry.Ai.png'
 					alt='Description of the image'
 				/>
+			</div>
+			<div className='footer-content'>
+				<p>Serving up culinary creativity with AI precision</p>
 				<p>Email: info@example.com</p>
 				<p>Phone: 123-456-7890</p>
 			</div>
