16007770d0a013c7969f539675e32b3ac9649e41
HenrySaber <hsabe026@uottawa.ca>
2023-06-24 16:18:29 -0400

    s

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,8 +5,7 @@ const Footer = () => {
   return (
     <footer className="marketing-footer">
       <div className="footer-content">
-        <h3>Contact Us</h3>
-        <p>123 Main Street, City</p>
+        <img src="Hungry.Ai.png" alt="Description of the image" />
         <p>Email: info@example.com</p>
         <p>Phone: 123-456-7890</p>
       </div>
@@ -15,3 +14,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
