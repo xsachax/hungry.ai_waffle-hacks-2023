761dc1ef2be056d66f472e6ff457fc020f7a8b41
HenrySaber <hsabe026@uottawa.ca>
2023-06-25 06:26:39 -0400

    fix: removed comented code

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -8,39 +8,6 @@ const Result = (data) => {
   data = Object.values(data)[0];
   const arr = JSON.parse(data);
 
-  console.log(arr);
-  //console.log(myObject["choices"][0]["text"]);
-
-  //console.log(data);
-  //console.log(typeof data);
-  //const colors = ["#f7a1a9", "#9ea9f7", "#fad791", "#b697fc", "#f7c183"];
-  /*const dummyData = [
-    {
-      name: "Sansotei Ramen",
-      address: "1537 Merivale Rd, Ottawa, ON K2G 3J3, Canada",
-      website: "http://www.sansotei.com/",
-      review:
-        "Sansotei Ramen offers an authentic and satisfying ramen experience with rich broths, perfectly cooked noodles, and delicious toppings.",
-      rating: 9,
-    },
-    {
-      name: "Hokkaido Ramen Santouka",
-      address: "50 Rideau St, Ottawa, ON K1N 9J7, Canada",
-      website: "https://www.santouka.co.jp/en/",
-      review:
-        "Hokkaido Ramen Santouka serves outstanding tonkotsu ramen with creamy broth, impeccable noodles, and a taste of Hokkaido in every bite.",
-      rating: 8.5,
-    },
-    {
-      name: "Ken's Sushi House",
-      address: "256 Bank St, Ottawa, ON K2P 1X4, Canada",
-      website: null,
-      review:
-        "Ken's Sushi House surprises with a delightful bowl of ramen alongside its sushi selection, featuring flavorful broth and customizable toppings.",
-      rating: 8,
-    },
-  ];*/
-
   return (
     <div className="flex gap-2 p-4 justify-center items-start flex-wrap">
       {arr == null ? (
